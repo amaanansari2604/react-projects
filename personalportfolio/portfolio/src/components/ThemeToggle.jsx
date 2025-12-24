a1514a8650b65ce9b7ef6aebca3d432c324a57dc
amaanansari2604 <amaanansari790786@gmail.com>
2025-12-24 17:24:59 +0530

    utils added

--- personalportfolio/portfolio/src/components/ThemeToggle.jsx
+++ personalportfolio/portfolio/src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Sun, Moon} from 'lucide-react';
+import { cn } from "../lib/utils";
 
 export function ThemeToggle(){
     const [isDarkMode, setIsDarkMode] = useState(false);
@@ -26,7 +27,11 @@ export function ThemeToggle(){
         }
     }
     return <div>
-        <button onClick={toggleTheme}>{isDarkMode ?
+        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+            "focus:outline-hidden"
+        )}>
+            
+            {isDarkMode ?
          (<Sun className="h-6 w-6 text-yellow-300"/>) : 
          (<Moon className="h-6 w-6 text-blue-300" />)}</button>
     </div>

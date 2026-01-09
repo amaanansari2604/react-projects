76c167354d2c72dfc1d97fc312af447931703294
amaanansarii <amaanansari790786@gmail.com>
2026-01-09 08:25:37 +0530

    game header card and app.jsx changes done.

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,11 +1,34 @@
+import { GameHeader } from "./components/GameHeader";
+import { Card } from "./components/Card";
+import { useState } from "react";
 
+const cardValues = [
+   "🍎", "🍌", "🍉", "🍇", "🍓", "🍒", "🍑", "🍍",
+   "🍎", "🍌", "🍉", "🍇", "🍓", "🍒", "🍑", "🍍"
+]
 
 function App() {
 
+  const [cards, setCards] = useState([])
+
+  const initializeGame = () => {
+    // shuffle the cards
+
+    const finalCards = cardValues.map((value, index)=>(
+        
+    ))
+  }
+
   return (
-      <>
-    Hello world
-    </>
+    <div className="app">
+      <GameHeader score={2} moves={5} />
+
+      <div className="cards-grid">
+        {cardValues.map((card) => (
+          <Card card={card}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ memory-game/src/components/Card.jsx
@@ -0,0 +1,11 @@
+
+export const Card = (card) => {
+    return (
+    <div className="card">
+        <div className="card-front">?</div>
+        <div className="card-back">
+            {card.card}
+        </div>
+        </div>
+    )
+}

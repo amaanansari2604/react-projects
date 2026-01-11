3a1d69299ac4a539c2dd3b0f28216133e5cd0b25
amaanansarii <amaanansari790786@gmail.com>
2026-01-11 07:50:46 +0530

    added functionality

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -3,28 +3,28 @@ import { Card } from "./components/Card";
 import { useEffect, useState } from "react";
 
 const cardValues = [
-   "🍎", "🍌", "🍉", "🍇", "🍓", "🍒", "🍑", "🍍",
-   "🍎", "🍌", "🍉", "🍇", "🍓", "🍒", "🍑", "🍍"
+  "🍎", "🍌", "🍉", "🍇", "🍓", "🍒", "🍑", "🍍",
+  "🍎", "🍌", "🍉", "🍇", "🍓", "🍒", "🍑", "🍍"
 ]
 
 function App() {
 
   const [cards, setCards] = useState([])
 
-    console.log(cardValues,"cardvalues")
+  console.log(cardValues, "cardvalues")
 
 
   const initializeGame = () => {
     // shuffle the cards
 
 
-    const finalCards = cardValues.map((value, index)=>(
-        {
-          id: index,
-          value,
-          isFlipped: false,
-          isMatched: false
-        }
+    const finalCards = cardValues.map((value, index) => (
+      {
+        id: index,
+        value,
+        isFlipped: false,
+        isMatched: false
+      }
     ))
 
     setCards(finalCards)
@@ -32,34 +32,40 @@ function App() {
 
   useEffect(() => {
     initializeGame();
-  },[])
+  }, [])
 
-const handleCardClick = (card) => {
-  // don't allow card is already flipped
+  const handleCardClick = (card) => {
+    // don't allow card is already flipped
 
-  if (card.isFlipped || card.isMatched){
-    return;
-  }
-
-  const newCards = cards.map((c) => {
-    if(c.id === card.id){
-      return {...c, isFlipped : true};
-    } else {
-      return c;
+    console.log(card, "-----card---handleccard click")
+    if (card.isFlipped || card.isMatched) {
+      return;
     }
-  });
 
-  setCards(newCards)
-}
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards)
+  }
   return (
     <div className="app">
       <GameHeader score={2} moves={5} />
 
       <div className="cards-grid">
         {cards.map((card) => (
-          <Card card={card} onClick={handleCardClick}/>
+          <Card
+            key={card.id}
+            card={card}
+            onClick={handleCardClick}
+          />
         ))}
       </div>
+y
     </div>
   )
 }
--- memory-game/src/components/Card.jsx
+++ memory-game/src/components/Card.jsx
@@ -1,11 +1,11 @@
-
-export const Card = (card, onClick) => {
-    return (
-    <div className={`card ${card.isFlipped ? "flipped": ""}`} onClick={() => onClick(card)}>
-        <div className="card-front">?</div>
-        <div className="card-back">
-            {card.card.value}
-        </div>
-        </div>
-    )
-}
+export const Card = ({ card, onClick }) => {
+  return (
+    <div
+      className={`card ${card.isFlipped ? "flipped" : ""}`}
+      onClick={() => onClick(card)}
+    >
+      <div className="card-front">?</div>
+      <div className="card-back">{card.value}</div>
+    </div>
+  );
+};

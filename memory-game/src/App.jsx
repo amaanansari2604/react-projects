add18e223547a31c177f10aa616728fe865c2d69
amaanansarii <amaanansari790786@gmail.com>
2026-01-11 13:23:07 +0530

    igmt

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -56,6 +56,26 @@ function App() {
     setFlippedCards(newFlippedCards)
 
     //check if the two cards are flipped
+
+    if (flippedCards.length === 1){
+      const firstCard = cards[flippedCards[0]];
+
+      if(firstCard.value === card.value){
+        alert("match")
+      } else{
+        //flip back card 1 and card 2
+
+        const flippedBackCard = newCards.map((c) => {
+          if (newFlippedCards.includes(c.id) || c.id === card.id ) {
+            return {...c, isFlipped: false}
+          } else {
+            return c;
+          }
+        })
+
+        setCards(flippedBackCard)
+      }
+    }
   }
   return (
     <div className="app">

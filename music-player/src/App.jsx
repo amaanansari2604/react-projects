7d15e820100ac04cfbdc3485d3f6060d5530013c
amaanansarii <amaanansari790786@gmail.com>
2026-01-12 16:32:35 +0530

    new files added

--- music-player/src/App.jsx
+++ music-player/src/App.jsx
@@ -1,20 +1,25 @@
 import { MusicPlayer } from "./components/MusicPlayer"
+import { BrowserRouter, Routes, Route } from "react-router"
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <div className="app">
         {/* <Navbar></Navbar> */}
 
         <main className="app-main">
           <div className="player-section">
-            <MusicPlayer></MusicPlayer>
+            <MusicPlayer/>
           </div>
-          <div className="content-section"></div>
+          <div className="content-section">
+           <Routes>
+            <Route path="/" element={}/>
+           </Routes>
+          </div>
         </main>
       </div>
-    </>
+      </BrowserRouter>
   )
 }
 

50d92bc3c2e348cf1d90223fa1462da1ace953ea
amaanansarii <amaanansari790786@gmail.com>
2026-01-13 08:22:47 +0530

    igmt

--- music-player/src/components/MusicPlayer.jsx
+++ music-player/src/components/MusicPlayer.jsx
@@ -1,4 +1,21 @@
+import { useMusic } from "../hooks/useMusic"
 
 export function MusicPlayer (){
-    return <div></div>
+    const {currentTrack} = useMusic();
+    return (
+        <div className="music-player">
+            <audio src=""></audio>
+
+            <div className="track-info">
+                <h3 className="track-title">{currentTrack.title}</h3>
+                <p className="track-artist">{currentTrack.artist}</p>
+            </div>
+
+            <div className="progress-container">
+                <span className="time">
+                    
+                </span>
+            </div>
+        </div>
+    )
 }

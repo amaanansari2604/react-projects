e1289585bd4392db03dca9dbf5236944f0387860
amaanansarii <amaanansari790786@gmail.com>
2026-01-20 11:12:31 +0530

    added navbar functionality

--- music-player/src/components/Playlists.jsx
+++ music-player/src/components/Playlists.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react"
 
 
 export function Playlists (){
-    return <div>plyalists</div>
+
+    const [newPlaylist, setNewPlaylist] = useState("");
+
+    const handleCreatePlaylist = () => {
+
+    }
+
+
+    return <div className="playlists">
+        <h2> Playlists</h2>
+
+        {/* create the playlist */}
+
+        <div className="create-playlist">
+            <h3>Create New Playlist</h3>
+            <div className="playlist-form">
+                <input type="text" placeholder="playlist name..." className="playlist-input" onChange={(e) => setNewPlaylist(e.target.value)} value={newPlaylist} />
+                <button className="create-btn" onClick={handleCreatePlaylist}>Create Playlist</button>
+            </div>
+        </div>
+    </div>
 }
